e28c9b8c9af12c00eaf709c04d2ad6935eee4285
sequispe <sequispe02@gmail.com>
2026-02-20 17:01:51 -0300

    Update admin.js

--- admin/admin.js
+++ admin/admin.js
@@ -51,11 +51,11 @@ async function cargarJSON() {
   const data = await res.json();
   shaActual = data.sha;
 
-  const contenido = atob(data.content);
-  textarea.value = contenido;
+const decoded = atob(data.content);
+const bytes = Uint8Array.from(decoded, c => c.charCodeAt(0));
+const textoCorrecto = new TextDecoder().decode(bytes);
 
-  estado.textContent = "Cargado ✅";
-}
+textarea.value = textoCorrecto;
 
 /* ============================= */
 /* GUARDAR JSON */
